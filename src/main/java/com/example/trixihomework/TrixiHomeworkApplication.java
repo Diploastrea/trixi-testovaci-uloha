e2e929e430adb621de05f5cf40e0fd781ffe7551
Daniel <23vietduc@gmail.com>
2022-10-31 18:03:59 +0100

    Initial commit

---
+++ src/main/java/com/example/trixihomework/TrixiHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.trixihomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrixiHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrixiHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trixihomework/TrixiHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trixihomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrixiHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
